923c8091df670bc3fa6cce2ba1aa7662e6e8d14e
rakshith <rakshit.s.gowda@gmail.com>
2022-11-08 22:26:20 +0530

    setup our project structure for the apppletree schools api

---
+++ cmd/api/main.go
@@ -0,0 +1,11 @@
+// filename : cmd/api/main.go
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("A journey of a thousnd miles begins with a single step..!!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module appletree
+
+go 1.19
